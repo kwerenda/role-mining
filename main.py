849361c868d64287aae8c3372387d182935dd2bb
bogna <bogna.bb+gh@gmail.com>
2014-12-17 01:55:54 +0100

    normalised mediators

--- main.py
+++ main.py
@@ -19,23 +19,94 @@ def mine(month, com):
     P.savefig("/Users/bogna/Documents/ED/projekt/04/enron{}_{}.png".format(month, com))
     return R
 
+
 if __name__ == '__main__':
     # mine(12, 3)
-    # n = Network("datasets/enron/timeslots/08-filtered3.edges", is_directed=False, use_communities=True)
-    # n = XNetwork("datasets/enron/timeslots/08-filtered3.edges",
-    #              communities_file="datasets/enron/communities/08-filtered3/k=3/directed_communities")
-    n = XNetwork("datasets/enron/timeslots/09-filtered3.edges",
-                 communities_file="datasets/enron/communities/09-filtered3/k=3/directed_communities")
+
+
+    month = 12
+    n = XNetwork("datasets/enron/timeslots/{:02d}-filtered2.edges".format(month),
+                 communities_file="datasets/enron/communities/{:02d}-filtered2/k=3/directed_communities".format(month))
     nodes_with_cbc = n.calculate_CBC()
 
+    cbc_by_node = {nid: cbc for nid, cbc in nodes_with_cbc}
 
-    guys = Reader.read_lines("datasets/enron/enron_guys.txt")
-    guys = {int(nid) : email for nid, email in guys}
+    guys_tuples = Reader.read_lines("datasets/enron/enron_guys.txt")
+    guys = {int(nid): email for nid, email in guys_tuples}
+    guys_by_email = {email: int(nid) for nid, email in guys_tuples}
 
-    i = 0
-    for n, cbc in nodes_with_cbc:
-        i+=1
-        print i, n, cbc, guys[n]
+    all_cdc = cbc_by_node.values()
+    P.hist(all_cdc)
+    P.title("Enron - Mediator score distribution in month {}".format(month))
+    P.xlabel("Mediator score")
+    P.ylabel("Frequency")
+    P.show()
+
+    important_emails = [
+        "jeff.dasovich@enron.com",
+        "susan.scott@enron.com",
+        "stanley.horton@enron.com",
+        "kenneth.lay@enron.com",
+        "mary.hain@enron.com",
+        "d..steffes@enron.com",
+        "k..allen@enron.com",
+        "john.zufferli@enron.com",
+        "kam.keiser@enron.com",
+        "m..presto@enron.com",
+        "jeff.skilling@enron.com",
+        "e..haedicke@enron.com",
+        "danny.mccarty@enron.com",
+        "chris.dorland@enron.com",
+        "barry.tycholiz@enron.com",
+        "j..kean@enron.com",
+        "shelley.corman@enron.com",
+        "j.kaminski@enron.com",
+        "louise.kitchen@enron.com",
+        "steven.harris@enron.com",
+        "mark.whitt@enron.com",
+        "drew.fossum@enron.com",
+        "gerald.nemec@enron.com",
+        "sally.beck@enron.com",
+        "tori.kuykendall@enron.com",
+        "john.arnold@enron.com",
+        "mike.grigsby@enron.com",
+        "tana.jones@enron.com",
+        "elizabeth.sager@enron.com",
+        "kay.mann@enron.com",
+        "bill.williams@enron.com",
+        "kim.ward@enron.com",
+        "stacy.dickson@enron.com",
+        "kate.symes@enron.com",
+        "lavorato@enron.com",
+        "taylor@enron.com",
+        "jason.williams@enron.com",
+        "kimberly.watson@enron.com",
+        "s..shively@enron.com",
+        "mike.swerzbin@enron.com",
+        "diana.scholtes@enron.com",
+        "teb.lokey@enron.com",
+        "rod.hayslett@enron.com",
+        "james.derrick@enron.com",
+        "scott.neal@enron.com",
+        "michelle.lokay@enron.com",
+        "paul.y.barbo@enron.com"
+    ]
+for email in important_emails:
+    try:
+        print "{}\t{:.4f}".format(email, cbc_by_node[guys_by_email[email]])
+    except KeyError:
+        print "{}\tX".format(email)
+
+i = 0
+print "month", month
+print "-----------------------------------"
+print "rank\tnode_id\temail\tMS"
+print "-----------------------------------"
+for n, cbc in nodes_with_cbc:
+    i += 1
+    if guys[n] not in important_emails:
+        print "{}\t{}\t{}\t{:.4f}".format(i, n, guys[n], cbc)
+
 
 
     # n.print_communities()
--- src/XNetwork.py
+++ src/XNetwork.py
@@ -2,12 +2,14 @@ import networkx as nx
 from itertools import combinations
 from Network import Network
 from pprint import pprint
+from collections import defaultdict
 
 class XNetwork(object):
     """Yet another Network class, this time based on NetworkX"""
     def __init__(self, edges_file, communities_file=None, use_communities=True):
         self.graph = nx.read_edgelist(edges_file, create_using=nx.MultiGraph(), nodetype=int)
         if use_communities:
+            self.communities = defaultdict(set)
             if communities_file:
                 comms = Network.get_communities_from_cf(communities_file)
                 self.create_communities(comms)
@@ -15,11 +17,13 @@ class XNetwork(object):
 
 
     def create_communities(self, communities_list):
+
         for node in self.graph.nodes():
             self.graph.add_node(node, comm=set())
         # pprint(self.graph.nodes(data=True))
         for node, community in communities_list:
             self.graph.node[node]['comm'].add(community)
+            self.communities[community].add(node)
 
 
     def shortest_paths(self, v1, v2):
@@ -29,57 +33,24 @@ class XNetwork(object):
         except:
             paths = []
         return paths
-        # try:
-        #     return
-        # except: # nx.NetworkXNoPath:
-        # # except nx.NetworkXNoPath:
-        #     return []
 
-
-    def calculate_CBC(self, is_directed=True):
+    def calculate_CBC(self, is_directed=False):
         """Calculate CBC for all nodes in network"""
         g = self.graph
-        for node in g.nodes():
-            g.add_node(node, cbc=0)   # initialise cdc = 0 for each node
+        for node in g.nodes():  # initialise cdc = 0 for each node
+            g.add_node(node, nbc=0)
+            g.add_node(node, ds_count=set())
 
         for v1, v2 in combinations(g.nodes(), 2):
-            print "calculating for ", v1, "and", v2
-            comms1 = g.node[v1]['comm']
-            comms2 = g.node[v2]['comm']
-            if comms1 and comms2 and comms1.isdisjoint(comms2):   # they are not the same community
+            # print "calculating for ", v1, "and", v2
+            communities1, communities2 = g.node[v1]['comm'], g.node[v2]['comm']
+            if communities1 and communities2 and communities1.isdisjoint(communities2):   # they are not the same community
                 for path in self.shortest_paths(v1, v2):
                     for v in path[1:-1]:
-                        g.node[v]['cbc'] += 1
+                        g.node[v]['nbc'] += 1.0/min([len(self.communities[x]) for x in communities1.union(communities2)])
+                        g.node[v]['ds_count'].update(communities1.union(communities2))
 
-        return sorted([[node, g.node[node]['cbc']] for node in g.nodes()], reverse=True, key=lambda e: e[1]);
-        # if not is_directed:
-        #     for v in self.graph.vertices():
-        #         self.graph.vp['CDC'][v] /= 2
-
-
-
-
-        #
-        # cdc = self.graph.new_vertex_property('int32_t')
-        # self.graph.vertex_properties['CDC'] = cdc
-        # #set all to 0
-        # for v in self.graph.vertices():
-        #         self.graph.vp['CDC'][v] = 0
-        #
-        # # dla kazdej pary wezlow w grafie - (kolejnosc ma znaczenie w skierowanych)
-        # for v1, v2 in product(self.graph.vertices(), self.graph.vertices()):
-        #     v1_lab, v2_lab = self.label2index[v1], self.label2index[v2]
-        #
-        #     # jezeli community (c1) != commmunity (C2)
-        #     if set(self.communities[v1_lab]).isdisjoint(set(self.communities[v2_lab])):
-        #         #   dla kazdej najkrotszej sciezki p od c1 do c2
-        #         for path in self.shortest_paths2(v1, v2):
-        #             for v in path:
-        #                 self.graph.vp['CDC'][v] += 1
-        #
-        # if not is_directed:
-        #     for v in self.graph.vertices():
-        #         self.graph.vp['CDC'][v] /= 2
-        #
-        #
-        #
+        mediator_score = [[node, g.node[node]['nbc']*len(g.node[node]['ds_count'])] for node in g.nodes()]
+        score_sum = sum([x[1] for x in mediator_score])
+        norm_ms = [[node, score/score_sum] for node, score in mediator_score]
+        return sorted(norm_ms, reverse=True, key=lambda e: e[1])
